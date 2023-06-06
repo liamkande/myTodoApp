b63a1027624d573e671601bc0202374d9d2a36e1
Liam Ansoumane Kadher Kande <liamkande@icloud.com>
2023-06-06 04:46:17 -0700

    added logic and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface TodoProps {
+  id: number
+  text: string
 }
 
-export default App;
+const TodoContainer = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`
+
+const TodoHeader = styled.h1`
+  text-align: center;
+`
+
+const TodoInput = styled.input`
+  width: 90%;
+  padding: 10px;
+  margin-bottom: 10px;
+`
+
+const TodoButton = styled.button`
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`
+
+const TodoList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`
+
+const TodoItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`
+
+const Todo: React.FC = () => {
+  const [todos, setTodos] = useState<TodoProps[]>([])
+  const [inputText, setInputText] = useState("")
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value)
+  }
+
+  const handleAddTodo = () => {
+    if (inputText.trim() === "") return
+
+    const newTodo: TodoProps = {
+      id: Date.now(),
+      text: inputText,
+    }
+
+    setTodos([...todos, newTodo])
+    setInputText("")
+  }
+
+  const handleRemoveTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  return (
+    <TodoContainer>
+      <TodoHeader>My Daily Todo App</TodoHeader>
+      <div>
+        <TodoInput
+          type="text"
+          placeholder="Add Todo's here..."
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <TodoButton onClick={handleAddTodo}>Add Todo</TodoButton>
+      </div>
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+          </TodoItem>
+        ))}
+      </TodoList>
+    </TodoContainer>
+  )
+}
+
+export default Todo
